eeebe6f48e995ac9b42fa01823647b76bb046257
elamathi <elamathi0407@gmail.com>
2020-04-05 15:55:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/toastapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.toastapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button Toast_btn;
+    Button Count_btn;
+    TextView Text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toast_btn = findViewById(R.id.toast);
+        Count_btn = findViewById(R.id.count);
+        Text = findViewById(R.id.disp);
+        Text.setText("0");
+
+
+        Toast_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Hello Toast ! ",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Count_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int c = Integer.parseInt(Text.getText().toString())+1;
+                Text.setText(""+c);
+            }
+        });
+
+
+
+
+
+    }
+}
